5d88ccb4bc74d0efdc6afd349a0b3f159194041f
Bhagirath P <bhagirath.p.9989@gmail.com>
2015-11-30 15:54:39 +0530

    added rvm installation script also

---
+++ install_rails_stack_rvm.sh
@@ -0,0 +1,14 @@
+# Install cURL
+sudo apt-get update
+sudo apt-get install -y curl
+
+
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+\curl -sSL https://get.rvm.io | bash -s stable --rails
+
+
+source ~/.rvm/scripts/rvm
+
+
+# Specify your required version
+rvm install 2.2.2 
