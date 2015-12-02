554641b5087faef9893e64be903727e62609fe15
Bhagirath P <bhagirath.p.9989@gmail.com>
2015-12-02 15:33:37 +0530

    installation procedure for centos

--- install_rails_stack_rbenv.sh
+++ install_rails_stack_rbenv.sh
@@ -2,16 +2,21 @@
 
 
 #=================================================================================
-# UBUNTU SYSTEM UPDATE
+# LINUX SYSTEM UPDATE
 #=================================================================================
 update_ubuntu_system () {
 	# Update your system
 	sudo apt-get update
 }
 
+update_centos_system () {
+	# Update your system
+	sudo yum update
+}
+
 
 #=================================================================================
-# UBUNTU GIT INSTALLATION
+# LINUX GIT INSTALLATION
 #=================================================================================
 install_ubuntu_git () {
 	# Call update_system
@@ -21,9 +26,15 @@ install_ubuntu_git () {
 	sudo apt-get install -y build-essential libssl-dev libcurl4-gnutls-dev libexpat1-dev gettext unzip
 }
 
+install_git_centos () {
+	# Update system
+	update_centos_system
+	sudo yum install -y git
+}
+
 
 #=================================================================================
-# UBUNTU POSTGRESQL INSTALLATION
+# LINUX POSTGRESQL INSTALLATION
 #=================================================================================
 install_ubuntu_postgres () {
 	# Call update_system
@@ -32,9 +43,18 @@ install_ubuntu_postgres () {
 	sudo apt-get install -y postgresql postgresql-contrib libpq-dev
 }
 
+install_centos_postgres () {
+	# Update system
+	update_centos_system
+	sudo yum install -y postgresql94-server
+	/usr/pgsql-9.4/bin/postgresql94-setup initdb
+	chkconfig postgresql-9.4 on
+	service postgresql-9.4 start
+}
+
 
 #=================================================================================
-# UBUNTU RUBY INSTALLATION
+# LINUX RUBY INSTALLATION
 #=================================================================================
 install_ubuntu_rails () {
 	# Call update_system
@@ -59,11 +79,42 @@ install_ubuntu_rails () {
 	gem install bundler
 }
 
+install_rails_centos () {
+	sudo yum install -y git-core zlib zlib-devel gcc-c++ patch readline readline-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison curl sqlite-devel
+	cd
+	git clone git://github.com/sstephenson/rbenv.git .rbenv
+	echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+	echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+	exec $SHELL
 
+	git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+	echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bash_profile
+	exec $SHELL
+
+	rbenv install -v 2.2.2
+	rbenv global 2.2.2
+
+	gem install bundler
+	gem install rails
+
+	sudo yum -y install epel-release
+	sudo yum -y install nodejs
+}
+
+
+#=================================================================================
+# MAIN INSTALLATION METHODS
+#=================================================================================
 set_up_rails_stack_linux () {
 	install_ubuntu_git
 	install_ubuntu_postgres
 	install_ubuntu_rails
+}
+
+set_up_rails_stack_centos () {
+	install_git_centos
+	install_centos_postgres
+	install_rails_centos
 }
 
 
@@ -74,23 +125,25 @@ OS="`uname`"
 case $OS in
   'Linux')
     OS='Linux'
-    echo $OS
-    set_up_rails_stack_linux
-    ;;
-  'FreeBSD')
-    OS='FreeBSD'
-    echo $OS
-    ;;
-  'WindowsNT')
-    OS='Windows'
-    echo $OS
+    # echo $OS
+    # set_up_rails_stack_linux
+    LINUX_FLAVOUR = $(python -mplatform | grep Ubuntu | echo ubuntu || echo centos)
+    if [ "$LINUX_FLAVOUR" == "linux" ];
+    	then
+    	if [ "$LINUX_FLAVOUR" == "ubuntu" ];
+			then
+				set_up_rails_stack_linux
+			elif [ "$LINUX_FLAVOUR" == "centos" ];
+			then
+				set_up_rails_stack_centos
+			else
+				echo "Unknown OS. Aborting Installation"
+			fi
+		fi
+	fi
     ;;
   'Darwin') 
     OS='Mac'
-    echo $OS
-    ;;
-  'SunOS')
-    OS='Solaris'
     echo $OS
     ;;
   *) ;;
