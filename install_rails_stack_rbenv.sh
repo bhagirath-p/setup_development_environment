eaf0143aec273abfa5104e430898b7f989b50c01
Bhagirath P <bhagirath.p.9989@gmail.com>
2015-12-02 14:42:37 +0530

    Update install_rails_stack_rbenv.sh

--- install_rails_stack_rbenv.sh
+++ install_rails_stack_rbenv.sh
@@ -43,12 +43,12 @@ install_ubuntu_rails () {
 	sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
 	cd
 	git clone git://github.com/sstephenson/rbenv.git .rbenv
-	echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
-	echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+	echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+	echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 
 	git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-	echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bash_profile
-	. ~/.bash_profile
+	echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+	. ~/.bashrc
 
 	# Install rails and set global version
 	rbenv install -v 2.2.3
